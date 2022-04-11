8b90394cdfb8fa9b155afacbc9ac8f80ac0885ba
MulletBoy42 <96805001+MulletBoy42@users.noreply.github.com>
2022-04-11 02:28:08 -0500

    something is wrong with this starter code

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <p>This is a warning, you are about to combust!</P>
+  `
+})
+
+export class WarningAlertComponent {
+
+}
