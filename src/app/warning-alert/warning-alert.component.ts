e272a21f67388dd37a9d4b4d1a85b47dc99b0ce6
MulletBoy42 <96805001+MulletBoy42@users.noreply.github.com>
2022-04-11 02:28:18 -0500

    more

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -4,7 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-warning-alert',
   template: `
   <p>This is a warning, you are about to combust!</P>
-  `
+  `,
+  styles: [
+    `
+    p {
+      padding: 20px;
+      background-color: mistyrose;
+      boarder: 1px solid red;
+    }
+    `
+  ]
 })
 
 export class WarningAlertComponent {
